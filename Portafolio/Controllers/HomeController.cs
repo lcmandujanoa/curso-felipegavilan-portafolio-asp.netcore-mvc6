ef78762439dc2b69386876aac79141c097b8e80d
Lucas <lcmandujanoa@gmail.com>
2022-08-21 22:32:34 -0500

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel()
+            {
+                 Proyectos = proyectos
+            };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            { 
+                new Proyecto{ Titulo = "Amazon", Descripcion = "E-Commerce realizado en ASP.NET Core", Link = "https://amazon.com", ImagenURL = "/imagenes/amazon.png" },
+                new Proyecto{ Titulo = "New York Times", Descripcion = "Página de noticias en React", Link = "https://nytimes.com", ImagenURL = "/imagenes/nyt.png" },
+                new Proyecto{ Titulo = "Reddit", Descripcion = "Red social para compartir en comunidades", Link = "https://reddit.com", ImagenURL = "/imagenes/reddit.png" },
+                new Proyecto{ Titulo = "Steam", Descripcion = "Tienda en línea para comprar videojuegos", Link = "https://store.steampowered.com", ImagenURL = "/imagenes/steam.png" }
+            };
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
